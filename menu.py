69ab942fcb93388c4c36dccfc80dce0dbc7e73dd
elPatronDB <diegobran@umes.edu.gt>
2025-02-26 11:41:46 -0600

    Se ha creado el menú,

---
+++ menu.py
@@ -0,0 +1,38 @@
+#from tabla_verdad import TablaVerdad
+import os
+
+class Menu:
+    # def __init__(self):
+    #     self.tabla = TablaVerdad()
+
+    def clearScreen(self):
+        os.system('cls')
+
+    def mostrar_menu(self):
+        while True:
+            print('\n               GENERADOR DE TABLAS DE VERDAD - DIEGO BRAN')
+            print('\n1. Generar tabla de verdad')
+            print('2. Salir')
+            try:
+                opcion = input('\nElige una opción: ').strip()
+                if not opcion:
+                    raise ValueError('No es posible la opción en blanco')
+                opcion = int(opcion)
+                if opcion == 1:
+                    self.tabla.ingresar_datos()
+                    self.tabla.generar_tabla()
+                elif opcion == 2:
+                    print('¡ADIOS!')
+                    break
+                else:
+                    print('Solamente puede escribir una de las dos opciones')
+            except ValueError as problema:
+                if str(problema) == 'La opción no puede estar vacía':
+                    print('Error: Debe ingresar una opción')
+                else:
+                    print('Error: Ingrese un número válido (1 o 2)')
+        
+
+menu = Menu()
+menu.mostrar_menu()
+
